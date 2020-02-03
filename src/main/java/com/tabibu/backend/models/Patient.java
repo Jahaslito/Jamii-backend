1bd8ab1de7158b277207884c531b59a48668f329
Baraka Mwakisha <barakamwakisha@gmail.com>
2020-02-03 17:14:00 +0300

    Complete Patient POJO model

--- src/main/java/com/tabibu/backend/models/Patient.java
+++ src/main/java/com/tabibu/backend/models/Patient.java
@@ -1,4 +1,51 @@
 package com.tabibu.backend.models;
 
 public class Patient {
+    private String name;
+    private int phone;
+    private String email;
+    private int age;
+    private String password;
+
+    public Patient() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
