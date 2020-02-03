6f4fc6cd5f7e748c0bfd4a26ab99da7a3a0428f1
Baraka Mwakisha <barakamwakisha@gmail.com>
2020-02-03 17:01:40 +0300

    Update dependencies and project structure setup

---
+++ src/main/java/com/tabibu/backend/TabibuBackendApplication.java
@@ -0,0 +1,12 @@
+package com.tabibu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabibuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabibuBackendApplication.class, args);
+    }
+}
