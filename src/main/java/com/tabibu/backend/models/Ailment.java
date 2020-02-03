b21397b7e926371e372e9f8d5c9b0337303f777a
Baraka Mwakisha <barakamwakisha@gmail.com>
2020-02-03 17:14:59 +0300

    Complete Ailment POJO model

--- src/main/java/com/tabibu/backend/models/Ailment.java
+++ src/main/java/com/tabibu/backend/models/Ailment.java
@@ -1,4 +1,29 @@
 package com.tabibu.backend.models;
 
 public class Ailment {
+    private String name;
+    private String comment;
+
+    public Ailment() {}
+
+    public Ailment(String name, String comment) {
+        this.name = name;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
