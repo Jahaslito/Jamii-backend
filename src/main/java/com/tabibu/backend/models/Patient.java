f34e25cfafb9f8be6474f1bf7811a9dc610f26f5
Baraka Mwakisha <barakamwakisha@gmail.com>
2020-02-03 17:12:42 +0300

    Complete Pharmacy POJO model

---
+++ src/main/java/com/tabibu/backend/models/Patient.java
@@ -0,0 +1,4 @@
+package com.tabibu.backend.models;
+
+public class Patient {
+}
---
+++ src/main/java/com/tabibu/backend/models/Pharmacy.java
@@ -0,0 +1,51 @@
+package com.tabibu.backend.models;
+
+public class Pharmacy {
+    private String name;
+    private String location;
+    private int phone;
+    private String username;
+    private String password;
+
+    public Pharmacy() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
